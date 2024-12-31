e025dc1a3cbfb87aebe6d280186ccbddf418d22b
Tarasus <98831090+TheTarasus@users.noreply.github.com>
2024-12-31 19:56:27 +0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
+    ws: 'wss://api.create-politica.su',
+    web: 'https://api.create-politica.su',
     // extraToken: token,
 };
 
